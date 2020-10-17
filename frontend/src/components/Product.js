1d00ff97dfaafe7588052a05e760b7c3074316b9
GWilczyk <gwilczyk.github@gmail.com>
2020-10-17 14:51:15 +0200

    HomeScreen Product Listing

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const Product = ({
+	_id,
+	image,
+	name,
+	description,
+	rating,
+	numReviews,
+	price
+}) => {
+	return (
+		<Card className='my-3 p-3 rounded'>
+			<a href={`/product/${_id}`}>
+				<Card.Img src={image} variant='top' />
+			</a>
+
+			<Card.Body>
+				<a href={`/product/${_id}`}>
+					<Card.Title as='div'>
+						<strong>{name}</strong>
+					</Card.Title>
+				</a>
+
+				<Card.Text as='div'>
+					<div className='my-3'>
+						{rating} from {numReviews} reviews
+					</div>
+				</Card.Text>
+
+				<Card.Text as='h3'>${price}</Card.Text>
+			</Card.Body>
+		</Card>
+	);
+};
+
+export default Product;
