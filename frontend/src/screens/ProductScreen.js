3b46644541356a7e59167cf81c82f777b578a2b5
GWilczyk <gwilczyk.github@gmail.com>
2020-10-20 12:56:16 +0200

    Implementing Quantity Select & Add To Cart Button

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { productDetails } from '../actions/productActions';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import {
+	Row,
+	Col,
+	Image,
+	ListGroup,
+	Card,
+	Button,
+	Form
+} from 'react-bootstrap';
 import Rating from '../components/Rating';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 
-const ProductScreen = ({ match }) => {
+const ProductScreen = ({ match, history }) => {
+	const [quantity, setQuantity] = useState(0);
+
 	const dispatch = useDispatch();
 
 	const { loading, error, product } = useSelector(
@@ -17,6 +27,10 @@ const ProductScreen = ({ match }) => {
 	useEffect(() => {
 		dispatch(productDetails(match.params.id));
 	}, [dispatch, match]);
+
+	const addToCartHandler = () => {
+		history.push(`/cart/${match.params.id}?qty=${quantity}`);
+	};
 
 	const {
 		image,
@@ -77,11 +91,33 @@ const ProductScreen = ({ match }) => {
 									</Row>
 								</ListGroup.Item>
 
+								{countInStock > 0 && (
+									<ListGroup.Item>
+										<Row>
+											<Col>Qty: </Col>
+											<Col>
+												<Form.Control
+													as='select'
+													value={quantity}
+													onChange={event => setQuantity(event.target.value)}
+												>
+													{[...Array(countInStock).keys()].map(num => (
+														<option key={num + 1} value={num + 1}>
+															{num + 1}
+														</option>
+													))}
+												</Form.Control>
+											</Col>
+										</Row>
+									</ListGroup.Item>
+								)}
+
 								<ListGroup.Item>
 									<Button
 										type='button'
 										className='btn-block'
 										disabled={countInStock === 0}
+										onClick={addToCartHandler}
 									>
 										Add To Cart
 									</Button>
