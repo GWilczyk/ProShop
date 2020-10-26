8a3a4b4c9795b0fb6f30fec4960b8dc365e7bc8f
GWilczyk <gwilczyk.github@gmail.com>
2020-10-26 22:52:55 +0100

    implementing User Login Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,8 +6,10 @@ import {
 	productDetailsReducer
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
+	userLogin: userLoginReducer,
 	productList: productListReducer,
 	productDetails: productDetailsReducer,
 	cart: cartReducer
@@ -17,8 +19,13 @@ const cartItemsFromStorage = localStorage.getItem('cartItems')
 	? JSON.parse(localStorage.getItem('cartItems'))
 	: [];
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+	? JSON.parse(localStorage.getItem('userInfo'))
+	: null;
+
 const initialState = {
-	cart: { cartItems: cartItemsFromStorage }
+	cart: { cartItems: cartItemsFromStorage },
+	userLogin: { userInfo: userInfoFromStorage }
 };
 
 const middleware = [thunk];
