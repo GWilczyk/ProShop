c7c8c0fd439ae3de5966a5f22c2b4fc82980a144
GWilczyk <gwilczyk.github@gmail.com>
2020-10-26 19:15:45 +0100

    Implementing Custom Authentication Middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,3 +22,22 @@ export const authUser = asyncHandler(async (req, res) => {
 		throw new Error('Invalid Email or Password');
 	}
 });
+
+// @desc   Get user profile
+// @route  GET /api/users/profile
+// @access Private
+export const getUserProfile = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id);
+
+	if (user) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin
+		});
+	} else {
+		res.status(404);
+		throw new Error('Invalid Email or Password');
+	}
+});
