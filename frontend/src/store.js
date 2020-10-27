b2d859ab98ba307f861eb8131890963d7b4cd2b6
GWilczyk <gwilczyk.github@gmail.com>
2020-10-27 12:51:26 +0100

    Added User Register Reducer, Action & Screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,13 +6,14 @@ import {
 	productDetailsReducer,
 	productListReducer
 } from './reducers/productReducers';
-import { userLoginReducer } from './reducers/userReducers';
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
 	cart: cartReducer,
 	productDetails: productDetailsReducer,
 	productList: productListReducer,
-	userLogin: userLoginReducer
+	userLogin: userLoginReducer,
+	userRegister: userRegisterReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
