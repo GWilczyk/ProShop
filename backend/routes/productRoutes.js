3dc60e837403eb7fa58c7f33ab48ee7447bead61
GWilczyk <gwilczyk.github@gmail.com>
2020-11-02 15:41:08 +0100

    Products Functionalities: Morgan & Create Review Endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,10 +6,12 @@ import {
 	deleteProduct,
 	getProducts,
 	getProductById,
-	updateProduct
+	updateProduct,
+	createProductReview
 } from '../controllers/productController.js';
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 router
 	.route('/:id')
 	.get(getProductById)
